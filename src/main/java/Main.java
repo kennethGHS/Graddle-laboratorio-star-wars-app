11d56577c216b2dab1fbf1fb7a640505e47c45f4
Jasson Rodriguez <jassonrm@icloud.com>
2021-03-19 18:09:51 -0600

    Add ending message

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,6 +11,7 @@ public class Main {
     public static void main(String args[]) throws IOException {
         Interface interfaz = new Interface();
         interfaz.executeFrame(getPersons());
+        System.out.println("Ending program");
     }
 
 }
